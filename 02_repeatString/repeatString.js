7309d8522253526bf9ef181c0c7b8a276bcf69d5
suix0 <112095627+suix0@users.noreply.github.com>
2023-12-07 22:24:46 +0800

    finished 02 js exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+  if (num === 0) {
+    return '';
+  } else if (num < 0) {
+    return 'ERROR';
+  }
 
+  let strRepeat = str;
+  for (let i = 1; i < num; i++) {
+    strRepeat += str;
+  }
+  return strRepeat;
 };
 
 // Do not edit below this line
