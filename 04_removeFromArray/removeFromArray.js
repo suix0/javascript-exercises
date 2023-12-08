d23c1614c16a96cb9d3d2438c4d66d26ba7a6552
suix0 <112095627+suix0@users.noreply.github.com>
2023-12-08 10:26:47 +0800

    finished 04 js exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+  for (const arg of args) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === arg) {
+        arr.splice(i, 1);
+      }
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
