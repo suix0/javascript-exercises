234444b1ded1be0928415a7edf9a9dc092c66411
suix0 <112095627+suix0@users.noreply.github.com>
2023-12-08 11:57:55 +0800

    finished tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  const celcius = (fahr - 32) / 1.8;
+  return parseFloat(celcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  const fahrenheit = (celcius * 1.8) + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
