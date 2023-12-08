51b474d3d1e20064e5ef83ead7ef7c117dfa52d7
suix0 <112095627+suix0@users.noreply.github.com>
2023-12-08 14:37:50 +0800

    finished titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i].title);
+  }
+  return newArr;
 };
 
 // Do not edit below this line
