9af10e91441d49cb22c21bf79520a38e885f202a
suix0 <112095627+suix0@users.noreply.github.com>
2023-12-08 09:43:04 +0800

    finished 03 reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  // Push the characters from the end of the string to new string
+  let strReverse = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    strReverse += str[i];
+  }
+  return strReverse;
 };
 
 // Do not edit below this line
