73cd9fd05ea5feeca5b157adce81237a9867a4f2
suix0 <112095627+suix0@users.noreply.github.com>
2023-12-08 14:27:09 +0800

    JUST FINISHED FIBONACCI ONG HOW DID I DO IT

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  num = Number(num);
+  if (num < 0) return 'OOPS';
 
+  arr = [1, 1];
+  for (let i = 1; i < num; i++) {
+    arr.push(arr[i] + arr[i - 1]);
+  }
+  return arr[num - 1];
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
