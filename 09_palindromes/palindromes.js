44ba54381dc519d175cbb7890b008d3c93e8aa68
suix0 <112095627+suix0@users.noreply.github.com>
2023-12-08 13:57:32 +0800

    finished palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let strUpper = str.toUpperCase(); // Convert string first to uppercase
+  let revisedStr = ''; // String without spaces and symbols
+  let reverseStr = ''; // The revised string in reverse
 
+  for (let i = 0; i < strUpper.length; i++) {
+    // Check if char is a letter
+    if (strUpper.charCodeAt(i) >= 65 && strUpper.charCodeAt(i) <= 90) {
+      revisedStr += strUpper[i];
+    // Also include numbers
+    } else if (strUpper.charCodeAt(i) >= 48 && strUpper.charCodeAt(i) <= 57) {
+      revisedStr += strUpper[i];
+    }
+  }
+
+  // Put the revised string in reverse
+  for (let i = revisedStr.length - 1; i >= 0; i--) {
+    reverseStr += revisedStr[i];
+  }
+  
+  return revisedStr === reverseStr;
 };
 
 // Do not edit below this line
