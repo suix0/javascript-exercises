b197df5ac4001cef19ca9a38402c99d2d922fa16
suix0 <112095627+suix0@users.noreply.github.com>
2023-12-08 11:06:48 +0800

    finished 05 sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if ((num1 < 0 || num2 < 0) || (typeof num1 != "number" || typeof num2 != "number")) {
+    return 'ERROR';
+  }
 
+  // Swap parameters if first parameter is greater than second parameter
+  if (num1 > num2) {
+    let bucket = num1;
+    num1 = num2;
+    num2 = bucket;
+  }
+
+  let sum = 0;
+  for (let i = num1; i <= num2; i++) {
+    sum += i;
+  }
+  return sum 
 };
 
 // Do not edit below this line
